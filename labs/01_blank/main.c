80d1e6c5de8c88dd09ffe8deb4eed9a1847cffeb
KGBcomrade <vlasov.sviatoslav@gmail.com>
2019-10-14 19:05:40 +0300

    mda

--- labs/01_blank/main.c
+++ labs/01_blank/main.c
@@ -4,8 +4,108 @@
  * is installed correctly.
  */
 
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+
+void gpio_config() {
+
+	/*LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
+	*/
+
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+    	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+
+
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_INPUT);	
+}
+
+static void rcc_config() {
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+    LL_RCC_HSI_Enable();
+    while (LL_RCC_HSI_IsReady() != 1);
+
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                LL_RCC_PLL_MUL_12);
+
+    LL_RCC_PLL_Enable();
+    while (LL_RCC_PLL_IsReady() != 1);
+
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+    SystemCoreClock = 48000000;
+}
+
+static void set_indicator(uint8_t num) {
+	
+	
+	static int32_t mask = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 \
+				| LL_GPIO_PIN_4 | LL_GPIO_PIN_5 |  LL_GPIO_PIN_6;
+
+
+	static const uint32_t decoder[] = {
+		LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | \
+		LL_GPIO_PIN_4 | LL_GPIO_PIN_5, // 0
+		LL_GPIO_PIN_1 | LL_GPIO_PIN_2, // 1
+		LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_6 | LL_GPIO_PIN_4 | \
+		LL_GPIO_PIN_3, // 2
+		LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_6 | LL_GPIO_PIN_2 | \
+		LL_GPIO_PIN_3, // 3
+		LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 // 4
+	};
+
+	uint32_t port_state = LL_GPIO_ReadOutputPort(GPIOB);
+
+	port_state = (port_state & ~mask) | decoder[num % (sizeof(decoder) / sizeof(uint32_t))];
+	LL_GPIO_WriteOutputPort(GPIOB, port_state);
+
+
+}	
+
+
+__attribute__ ((naked)) static void delay_10ms(void) {
+	asm("push {r7, lr}");
+	asm("ldr r6, [pc, #8]");
+	asm ("sub r6, #1");
+	asm ("cmp r6, #0");
+    	asm ("bne delay_10ms+0x4");
+    	asm ("pop {r7, pc}");
+    	asm (".word 0xea60"); 
+}
+
 int main(void)
 {
-    while (1);
-    return 0;
+	rcc_config();
+	gpio_config();
+	
+	set_indicator(2);
+
+	/*int status, pst = 0;
+	while (1) {
+		status = LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_0);
+		if(status && (status != pst)) {
+			delay_10ms();
+			LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+		}
+		pst = status;
+	}*/
+	return 0;
 }
